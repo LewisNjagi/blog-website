62bc7e3cfea029663d8c34ba3c16baee78f6be6d
Lewis Njagi <lewismaybach@gmail.com>
2021-03-09 00:44:28 +0300

    added quotes api

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://qyunky:Lewis860@localhost/blog'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    QUOTES_API_BASE_URL = ' http://quotes.stormconsultancy.co.uk/popular.json'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class prodConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+config_options = {
+'development':DevConfig,
+'production':prodConfig
+}
